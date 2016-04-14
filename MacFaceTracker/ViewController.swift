78824b03f5c4d7c0b8cf8d3b6fb2bf9d0337d786
markus <markus@torpvret.se>
2016-04-14 19:15:55 +0200

    First commit - empty project

--- MacFaceTracker/ViewController.swift
+++ MacFaceTracker/ViewController.swift
@@ -7,19 +7,38 @@
 //
 
 import UIKit
+import FaceTracker
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FaceTrackerViewControllerDelegate {
 
+    weak var faceTrackerViewController: FaceTrackerViewController?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "faceTrackerEmbed" {
+            faceTrackerViewController = segue.destinationViewController as? FaceTrackerViewController
+            faceTrackerViewController!.delegate = self
+        }
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        faceTrackerViewController?.startTracking { () -> Void in
+            
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    func faceTrackerDidUpdate(points: FacePoints?) {
+        
+    }
 }
 
